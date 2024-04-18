36157f1a452804a32d515c2537bba09cca937d08
Umme Habiba <habicoder@gmail.com>
2024-04-18 11:35:18 +0530

    added p14

---
+++ P14.py
@@ -0,0 +1,14 @@
+#docstrings
+def greet(name):
+    """
+    This function greets the user with the provided name.
+
+    Parameters:
+    name (str): The name of the user to greet.
+
+    Returns:
+    str: A greeting message.
+    """
+    return "Hello, " + name + "!"
+
+print(greet("Alice"))
